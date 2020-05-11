a8ec203b7e758f461387b0a6bc91e76b44bdd1df
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-12 04:50:15 +0530

    server validation of password

--- 030-jwt-login-script/check_user.php
+++ 030-jwt-login-script/check_user.php
@@ -1,5 +1,14 @@
 <?php
 
-echo json_encode("hello");
+$input = file_get_contents('php://input');
+$input = json_decode($input);
+//print_r($input);
+
+$response = "try again";
+if($input->email=="demo" && $input->password =="demo"){
+    $response = correct;
+}
+
+echo json_encode($response);
 
 ?>
