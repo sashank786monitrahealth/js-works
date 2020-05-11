07e62bc1770405eb4ee3f89177640d1a87b8ddec
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-11 16:31:13 +0530

    send data using fetch api

---
+++ 030-jwt-login-script/script.js
@@ -0,0 +1,26 @@
+let info = document.querySelector('#info');
+let email_input = document.querySelector('#email_input');
+let password_input = document.querySelector('#password_input');
+document.querySelector("#login_button").addEventListener("click",check_user);
+
+
+function check_user(){
+    let email = email_input.value;
+    let password = password_input.value;
+
+    if (email=='' || password=='') return;
+    console.log('continue..');
+    let post_data = {
+       "mode":"check_user",
+       "email":email,
+       "password": password
+    }
+
+    fetch("http://127.0.0.1:8080/check_user.php",
+    {
+        method:'post',
+        body:JSON.stringify(post_data),
+    });
+}
+
+
