c2df2873b6f0d13a7570339bf9d0321712c28b62
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-31 15:11:04 +0530

    addEventListeneres Callback

--- 031-test-callbacks/script.js
+++ 031-test-callbacks/script.js
@@ -8,3 +8,27 @@ setTimeout(logCall, 3000);
 setTimeout(function(){
     console.log("first staement");
 }, 2000);
+
+/*
+A very common application in JavaScript for callbacks are event listeners..
+*/
+
+let btn = document.querySelector("#button1");
+let h1Tag = document.querySelector("h1");
+
+
+
+btn.addEventListener("mousedown",function(e){
+    h1Tag.innerHTML = "Down";
+});
+
+
+
+btn.addEventListener("mouseup",function(e){
+    h1Tag.innerHTML = "Up";
+});
+
+
+btn.addEventListener("mouseout",function(e){
+    h1Tag.innerHTML = "Output";
+});
