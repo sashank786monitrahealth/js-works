52c908163f3ffb702e5ba881c8551b133f1165bb
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-01 13:02:34 +0530

    added setTimeout inside processStudents

--- 033-asynchronous-coding-callback/app.js
+++ 033-asynchronous-coding-callback/app.js
@@ -20,7 +20,7 @@ let processStudents = function(data,callback){
     for(let i=0; i<data.length;i++){
         if(data[i].school.toLowerCase() === "east"){
             if(typeof callback == "function"){
-                callback(data[i]);
+                setTimeout(callback(data[i]),0);
             }
         }
     }
