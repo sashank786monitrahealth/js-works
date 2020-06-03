ed70a6de4f241a39a261a019435399b960be2fad
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-03 07:43:51 +0530

    function returning promise

---
+++ 035-using-promises/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let asyncFunction = function(){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve("async function has resolved.")
+        },4000);
+    });
+};
+
+let asyncFunction2 = function(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve("asyncFunction2 is done.")
+        },3000);
+    });
+};
