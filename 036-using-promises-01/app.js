ef8936da226583b999697bce65ebd6f8c335b18f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-03 09:31:36 +0530

    this() method - use promise when resolved / failed

---
+++ 036-using-promises-01/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let asyncFunction = function() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve("asyncFunction has resolved.");
+        }, 4000);
+    });
+};
+
+let asyncFunction2 = function() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve("asyncFunction2 is done.");
+        }, 3000);
+    });
+};
+
+
+/*
+let wordnikWords = "http://api.wordnik.com/v4/words.json",
+wordnikWord = "http://api.wordnik.com/v4/word.json",
+apiKey = "?api_key=lksjldkfjlksdfklsflksjlfjslk",
+wordObj;
+*/
+
+let promise = asyncFunction();
+
+/*we can use the then method - when the promise is resolved. Or when it fails!*/
+
+promise.then();
