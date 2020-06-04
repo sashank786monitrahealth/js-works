ecf2738bd451f48505bc4a73f19cf51644df9c0a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-04 16:29:39 +0530

    test async

--- 038-promise-jsonplaceholder/app.js
+++ 038-promise-jsonplaceholder/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json));
+fetch('https://jsonplaceholder.typicode.com/todos/')
+  .then(data => data.json())
+  .then(obj => console.log(obj));
+
+  console.log("other code");
