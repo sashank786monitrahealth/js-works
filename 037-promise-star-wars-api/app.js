185b58a07c76cca7adc94cec03278f3361f4be05
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-04 16:15:17 +0530

    return promise from fetch api

--- 037-promise-star-wars-api/app.js
+++ 037-promise-star-wars-api/app.js
@@ -4,7 +4,12 @@ const swapi = function(num){
      let url = "https://swapi.dev/api/people/";
 
      fetch(url+num+"/").then(data => data.json())
-     .then(obj => console.log(obj));
+     .then(obj => {
+         console.log(obj);
+         return fetch(obj.homeworld)
+        })
+        .then(hwdata => hwdata.json())
+        .then(hwObject => console.log(hwObject));
 };
 
 
