6be4a2288c57d7d0041ca41e2093642f0b37e578
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-04 16:41:50 +0530

    testing for catch

--- 038-promise-jsonplaceholder/app.js
+++ 038-promise-jsonplaceholder/app.js
@@ -12,7 +12,7 @@ let todo = {
     title:"ee ho! essel world"
 }
 
-fetch('https://jsonplaceholder.typicode.com/todos/',{
+fetch('https://sonplaceholder.typicode.com/todos/',{
     method:'POST',
     headers:{
         'Content-Type':"application/json"
