2bbd964e221dcac8fbbed5293b5544e6ba46cfef
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-04 16:39:23 +0530

    use POST method with fetch api

--- 038-promise-jsonplaceholder/app.js
+++ 038-promise-jsonplaceholder/app.js
@@ -1,8 +1,27 @@
 "use strict";
 
-
-fetch('https://jsonplaceholder.typicode.com/todos/')
+/*
+fetch('https://jsonplaceholder.typicode.com/todos/5/')
   .then(data => data.json())
   .then(obj => console.log(obj));
+*/
+
+let todo = {
+    completed: false,
+    userID:1,
+    title:"ee ho! essel world"
+}
+
+fetch('https://jsonplaceholder.typicode.com/todos/',{
+    method:'POST',
+    headers:{
+        'Content-Type':"application/json"
+    },
+    body: JSON.stringify(todo)
+})
+.then(resp => resp.json())
+.then(obj => console.log(obj))
+.catch(reject => console.log(`Unable to create todo: ${reject}`));
+
 
   console.log("other code");
