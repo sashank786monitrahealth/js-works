c46fe414fa6608bedf7e68f860698bd6aee4bf56
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-05 12:42:08 +0530

    closure setup

---
+++ 041-JS-traditional-module-pattern/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function communication(){
+    let greet = "hello",
+    goodbye = "So long";
+
+    const getGreet = function(){
+        let d = new Date();
+        if (d.toLocaleTimeString().includes('AM')){
+            greet = "Good Morning"
+        } else {
+            greet = "Hello";
+        }
+
+        return greet;
+    };
+
+    const greeting = function(name) {
+        console.log(`${getGreet() + name}! Welcome to the course.`);
+    };
+};
