35e8d4939b783b1d06568a7fc1aff3015f5423d8
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-06 13:16:34 +0530

    promises exercise 2

--- 042-promises-exercise-02/app.js
+++ 042-promises-exercise-02/app.js
@@ -8,10 +8,27 @@ var MAINAPP = (function(nsp){
     (e.g. nsp.posts = posts & return nsp). Because the code is asynchronous - you will need to consider the best way to do that. 
     */
 
+    fetch(url+'posts/')
+    .then(response1 => response1.json() )
+    .then(posts => nsp.posts = posts)
+    .catch(err => console.log(`Problem retreiving posts: ${err}`));
+
+    fetch(url+"comments/")
+    .then(response2 => response2.json())
+    .then(comments => nsp.comments = comments)
+    .catch(reject => console.log(`Problem receiving comments: ${reject}`));
+
+    fetch(url+"todos/")
+    .then(response3 => response3.json())
+    .then(todos => nsp.todos = todos)
+    .catch(reject => console.log(`Problem receiving todos: ${reject}`));
+
     // public 
-    nsp.posts = posts;
-    nsp.comments = comments;
-    nsp.todos = todos;
+    //nsp.posts = posts;
+    //nsp.comments = comments;
+    //nsp.todos = todos;
 
     return nsp;
 })(MAINAPP || {});
+
+console.log(MAINAPP);
