cace830e010559340a6fd49e7f2856507e2cad2a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-06 08:18:44 +0530

    set the url to get us started

--- 042-promises-exercise-02/app.js
+++ 042-promises-exercise-02/app.js
@@ -1,9 +1,7 @@
 var MAINAPP = (function(nsp){
     "use strict";
 
-    let posts = [],
-    comments = [],
-    todos = [];
+    let url = "https://jsonplaceholder.typicode.com/";
 
     /*
     This IIFE is the start of an application. The first thing we want to do is download all the posts, comments and todos so that we can work with them. Add the code in order to do that. Also, make sure that you add the posts, comments and todos to the MAINAPP variable so they are accessible outside this function 
