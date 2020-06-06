01e7bfa59c7d7aa2566b75b6d234d78023b83b08
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-06 14:33:45 +0530

    promise done

--- 043-JS-creating-promises/app.js
+++ 043-JS-creating-promises/app.js
@@ -1,5 +1,18 @@
 "use strict";
 
 // create promise using a Promise constructor
-let a = new Promise();
+// now in the process of creating that promise you need to pass in a callback function
+// in the paranthesis
+// and the purpose of the callback function is to setup what the promise is going to do.
+// lets add a callback function
+// the callback function will have two parameters
+// 1. resolve - this function will be called when a promise is resolved.
+// 2. reject - this will be called when the promise is rejected.
+let a = new Promise(function(resolve, reject){
+      setTimeout(function(){
+          resolve("Done.")
+      },4000);
+});
+
+console.log(a);
 
